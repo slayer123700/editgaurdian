ea86097558bb9070e2d75bf2660615b27ef2807b
slayer <meerutuniversity996@gmail.com>
2025-05-22 16:36:05 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+TOKEN = "YOUR_BOT_TOKEN"
+ADMIN_IDS = [123456789]  # Replace with your Telegram ID
+BOT_NAME = "Edit-chan ✨"
+BOT_OWNER_USERNAME = "your_username"  # Without @
+SUPPORT_GROUP_LINK = "https://t.me/your_support_group"
