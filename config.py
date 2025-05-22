96f0ef00f4e3ecaf05d5bd50a521dff14bc55879
slayer <meerutuniversity996@gmail.com>
2025-05-22 16:38:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-TOKEN = "YOUR_BOT_TOKEN"
-ADMIN_IDS = [123456789]  # Replace with your Telegram ID
-BOT_NAME = "Edit-chan ✨"
-BOT_OWNER_USERNAME = "your_username"  # Without @
-SUPPORT_GROUP_LINK = "https://t.me/your_support_group"
+import os
+
+TOKEN = os.environ.get("BOT_TOKEN")
+ADMIN_IDS = list(map(int, os.environ.get("ADMIN_IDS", "123456789").split(",")))
+BOT_NAME = os.environ.get("BOT_NAME", "Edit-chan ✨")
+BOT_OWNER_USERNAME = os.environ.get("BOT_OWNER_USERNAME", "your_username")
+SUPPORT_GROUP_LINK = os.environ.get("SUPPORT_GROUP_LINK", "https://t.me/your_support_group")
