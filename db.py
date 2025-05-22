34f65e8a685986c3f2e364d5558b7b70581550db
slayer <meerutuniversity996@gmail.com>
2025-05-22 17:56:38 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,35 +2,38 @@ from pymongo import MongoClient
 from config import MONGO_URI
 
 client = MongoClient(MONGO_URI)
-db = client.edit_guardian
+db = client["edit_guardian_bot"]
 
-async def add_user(user_id):
-    db.users.update_one({"user_id": user_id}, {"$set": {"user_id": user_id}}, upsert=True)
+users_col = db["users"]
+groups_col = db["groups"]
+restricted_col = db["restricted"]
+settings_col = db["settings"]
 
-async def add_chat(chat_id):
-    db.chats.update_one({"chat_id": chat_id}, {"$set": {"chat_id": chat_id}}, upsert=True)
+# User Functions
+def add_user(user_id):
+    users_col.update_one({"_id": user_id}, {"$set": {"_id": user_id}}, upsert=True)
 
-async def get_all_users():
-    return [u["user_id"] for u in db.users.find()]
+def get_all_users():
+    return [doc["_id"] for doc in users_col.find()]
 
-async def get_all_chats():
-    return [c["chat_id"] for c in db.chats.find()]
+# Group Functions
+def add_group(chat_id):
+    groups_col.update_one({"_id": chat_id}, {"$set": {"_id": chat_id}}, upsert=True)
 
-async def add_restricted_user(user_id):
-    db.restricted.update_one({"user_id": user_id}, {"$set": {"user_id": user_id}}, upsert=True)
+def get_all_chats():
+    return [doc["_id"] for doc in groups_col.find()]
 
-async def remove_restricted_user(user_id):
-    db.restricted.delete_one({"user_id": user_id})
+# Restriction Functions
+def add_restricted_user(user_id):
+    restricted_col.update_one({"_id": user_id}, {"$set": {"_id": user_id}}, upsert=True)
 
-async def is_user_restricted(user_id):
-    return db.restricted.find_one({"user_id": user_id}) is not None
+def is_restricted(user_id):
+    return restricted_col.find_one({"_id": user_id}) is not None
 
-async def get_restricted_users():
-    return [u["user_id"] for u in db.restricted.find()]
+# Edit Delay Settings
+def get_edit_delay(chat_id):
+    data = settings_col.find_one({"_id": chat_id})
+    return data.get("delay", 5) if data else 5
 
-async def set_delay(chat_id, seconds):
-    db.delays.update_one({"chat_id": chat_id}, {"$set": {"seconds": seconds}}, upsert=True)
-
-async def get_delay(chat_id):
-    data = db.delays.find_one({"chat_id": chat_id})
-    return data["seconds"] if data else 5
+def set_edit_delay(chat_id, delay):
+    settings_col.update_one({"_id": chat_id}, {"$set": {"delay": delay}}, upsert=True)
